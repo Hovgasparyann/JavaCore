6502f7a511fcb7f6631a7031c3b9f12880d24bb6
Hovgasparyann <gevorgyan.1@mail.ru>
2021-10-15 19:20:29 +0400

    ArrayUtil

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -31,18 +31,62 @@ package homework.arrayutil;
 // }
 
 // մասիվի ամենափոքրը թիվը
-public class ArrayUtil {
-    public static void main(String[] args) {
-        int[] array = {5, 6, 13, 22, 49, 3, 54, 77, 16, 81};
-        int min = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            }
-        }
-        System.out.println(" " + min);
-    }
-}
+//public class ArrayUtil {
+//    public static void main(String[] args) {
+//        int[] array = {5, 6, 13, 22, 49, 3, 54, 77, 16, 81};
+//        int min = array[0];
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] < min) {
+//                min = array[i];
+//            }
+//        }
+//        System.out.println(" " + min);
+//    }
+//}
+
+// մասիվի բոլոր զույգ էլեմենտները
+
+//public class ArrayUtil {
+//    public static void main(String[] args) {
+//        int[] array = {5, 6, 13, 22, 49, 3, 54, 77, 16, 81};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0) {
+//                System.out.println(array[i] + "  ");
+//
+//            }
+//        }
+//    }
+//}
+
+// մասիվի բոլոր կենտ էլեմենտները
+
+//public class ArrayUtil {
+//    public static void main(String[] args) {
+//        int[] array = {5, 6, 13, 22, 49, 3, 54, 77, 16, 81};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i]%2!=0){
+//                System.out.println(array[i] + " ");
+//            }
+//
+//        }
+//    }
+//}
+
+// մասիվի էլեմենտների գումարը։
+
+//public class ArrayUtil {
+//    public static void main(String[] args) {
+//        int[] array = {5, 6, 13, 22, 49, 3, 54, 77, 16, 81};
+//        int a = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            a += array[i];
+//
+//        }
+//        System.out.println("array = " + a);
+//    }
+//}
+
+
 
 
 
