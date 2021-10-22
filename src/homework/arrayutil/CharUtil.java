465a63e2da3b1e1fb8a388a77b677cb3f11acb72
Hovgasparyann <gevorgyan.1@mail.ru>
2021-10-22 16:46:07 +0400

    CharUtil

---
+++ src/homework/arrayutil/CharUtil.java
@@ -0,0 +1,68 @@
+package homework.arrayutil;
+
+public class CharUtil {
+    public static void main(String[] args) {
+        // o quantity:
+       char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+//        int qanak = 0;
+//        for (int i = 0; i < bolola.length; i++) {
+//            char c = bolola[i];
+//            if (c == 'o') {
+//                qanak++;
+//            }
+//        }
+//        System.out.println(qanak);
+//        System.out.println();
+
+        //avarge
+
+//        int a = bolola.length+1;
+//        int average = a/2;
+//        System.out.print(bolola[average -1]);
+//        System.out.print(bolola[average]);
+
+        //find last ly
+
+
+//                if (bolola[bolola.length -2] == 'l' && bolola[bolola.length -1] == 'y'){
+//                    System.out.print(true);
+//                }else {
+//                    System.out.print(false);
+//                }
+
+
+        // find b*b
+
+//        char[] babola = {'b', 'a', 'b', 'o', 'l', 'a'};
+//
+//        boolean bool = false;
+//
+//        for (int i = 0; i < babola.length; i++) {
+//            if (babola[i] == 'b' && babola[i + 2] == 'b') {
+//                bool = true;
+//                break;
+//            }
+//        }
+//        System.out.println(bool);
+
+        //filter CharArray
+
+//        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
+//        int count = 0;
+//        for (int i = 0; i < text.length; i++) {
+//            if (text[i] != ' ') {
+//                count++;
+//            }
+//        }
+//        char[] result = new char[count];
+//        int index = 0;
+//        for (int i = 0; i < text.length; i++) {
+//            if (text[i] != ' ') {
+//                result[index] = text[i];
+//                index++;
+//            }
+//        }
+//        System.out.println(result);
+
+    }
+}
